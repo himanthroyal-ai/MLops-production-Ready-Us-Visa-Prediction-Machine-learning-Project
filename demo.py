b1a04dabbcfd6cb4de319b06ae7383f4aaf51688
bharu <himanth@gmail.com>
2025-12-04 20:15:21 +0530

    utils added

---
+++ demo.py
@@ -0,0 +1,8 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+logging.info("Demo file is started")
+try:
+    a=2/0
+except Excection as e:
+            raise]USvisaException(e,sys)
